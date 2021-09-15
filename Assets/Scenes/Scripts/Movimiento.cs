032929eb3dd74915aac213ec81a4e7397ac3033d
miguelz0 <miguel.zuluaga@upb.edu.co>
2021-09-15 16:40:05 -0500

    Actualización fricción y peso
    Profe, en este proyecto tengo todo, lo del agujero negro sería activar el método llamado rastreo :)

--- Assets/Scenes/Scripts/Movimiento.cs
+++ Assets/Scenes/Scripts/Movimiento.cs
@@ -5,56 +5,74 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    public Vector2 posicion;    
-    public Vector2 desplazar;    
+    [SerializeField]private Vector2 posicion;    
+    public Vector2 desplazar;
+    public Vector2 fuerza, sumatoria, friccion;
+    public float masa=1;
     public float maximoY=4.5f, minimoY=-4.5f, maximoX=4.5f, minimoX=-4.5f;
-    public Vector2 velocidad, aceleracion;
-    Vector2 velmax = new Vector2(49, 49);
+    public Vector2 velocidad, aceleracion,peso;
+    public Transform objetivo;
+    public float rebote, coeficiente=0.5f;
+    public bool bordes=true; 
+    Vector2 velmax = new Vector2(15, 15);
     void Start()
-    {              
-
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        ResetearAceleracion();
+        CheckRapidez();
         transform.position = posicion;
         posicion.Draw(Color.black);
         velocidad.Draw(posicion,Color.blue);
         aceleracion.Draw(posicion,Color.green);
         desplazar.Draw(posicion,Color.cyan);
-        Walker();
-        CheckRapidez();
+        Gravedad();
+        Friccion();
+        AplicarFuerza();        
+        Walker();      
+        //Rastreo();       
 
-        
-    }   
-    public void NuevaPosicion()
-    {
-        posicion = posicion + desplazar;
-        if (posicion.x<minimoX && posicion.x<maximoX)
-        {
-            desplazar.x = -desplazar.x;
-        }
-        if (posicion.x>maximoX &&posicion.x>minimoX)
-        {
-            desplazar.x = -desplazar.x;           
-        }
-        if(posicion.y<minimoY && posicion.y < maximoY)
-        {
-            desplazar.y = -desplazar.y;
-        }
-        if(posicion.y>maximoY && posicion.y > minimoY)
-        {
-            desplazar.y = -desplazar.y;
-        }        
         
     }
-    public void Walker()
+    public void ResetearAceleracion()
     {
-        posicion = posicion + velocidad * Time.deltaTime;
-        
+        aceleracion.x = 0;
+        aceleracion.y = 0;
+    }
+    public void NuevaPosicion()
+    {
+        if (bordes == true)
+        {
+            posicion = posicion + desplazar;
+            if (posicion.x < minimoX && posicion.x < maximoX)
+            {
+                desplazar.x = -desplazar.x;
+            }
+            if (posicion.x > maximoX && posicion.x > minimoX)
+            {
+                desplazar.x = -desplazar.x;
+            }
+            if (posicion.y < minimoY && posicion.y < maximoY)
+            {
+                desplazar.y = -desplazar.y;
+            }
+            if (posicion.y > maximoY && posicion.y > minimoY)
+            {
+                desplazar.y = -desplazar.y;
+            }
 
-        
+        }
+
+    }
+    public void Walker()
+    {        
+        velocidad = velocidad + aceleracion * Time.deltaTime;
+        posicion = posicion + velocidad * Time.deltaTime;       
+               
         if (velocidad.x < velmax.x && velocidad.y < velmax.y)
         {
             velocidad = velocidad + aceleracion * Time.deltaTime;
@@ -63,37 +81,57 @@ public class Movimiento : MonoBehaviour
         {
             velocidad = velmax;
         }
-        
-        if (posicion.x < minimoX || posicion.x > maximoX)
+        if (bordes == true)
         {
-            velocidad.x= - velocidad.x;
-            //aceleracion.x = -aceleracion.x;
-            if (posicion.x > maximoX)
+            if (posicion.x < minimoX || posicion.x > maximoX)
             {
-                posicion.x = 4.5f;
+                velocidad.x = -velocidad.x * rebote;
+
+                if (posicion.x > maximoX)
+                {
+                    posicion.x = 4.5f;
+                }
+                if (posicion.x < minimoX)
+                {
+                    posicion.x = -4.5f;
+                }
             }
-            if (posicion.x < minimoX)
+            if (posicion.y < minimoY || posicion.y > maximoY)
             {
-                posicion.x = -4.5f;
-            }
-        }        
-        if (posicion.y < minimoY || posicion.y > maximoY)
-        {
-            velocidad.y = -velocidad.y;
-            //aceleracion.y = -aceleracion.y;
-            if (posicion.y > maximoY)
-            {
-                posicion.y = 4.5f;
-            }
-            if (posicion.y < minimoY)
-            {
-                posicion.y = -4.5f;
-            }
+                velocidad.y = -velocidad.y * rebote;
+
+                if (posicion.y > maximoY)
+                {
+                    posicion.y = 4.5f;
+                }
+                if (posicion.y < minimoY)
+                {
+                    posicion.y = -4.5f;
+                }
+
+            }            
 
         }        
 
 
     }
+    public void Gravedad()
+    {
+        Vector2 gravedad = new Vector2(0, -9.8f);        
+        peso = masa *gravedad;
+
+    }
+    public void Friccion()
+    {        
+        friccion = -coeficiente*velocidad.normalized;
+    }
+    public void AplicarFuerza()
+    {        
+        sumatoria = fuerza + peso + friccion;       
+        aceleracion = (sumatoria / masa);
+    }
+    
+    
     public void CheckRapidez()
     {
         if (velocidad.magnitude > velmax.magnitude)
@@ -102,7 +140,14 @@ public class Movimiento : MonoBehaviour
             velocidad*= velmax;
         }
     }
+    
         
+    public void Rastreo()
+    {
+        aceleracion.x = objetivo.position.x- posicion.x;
+        aceleracion.y = objetivo.position.y- posicion.y;
 
+    }
+    
     
 }
